f851fc3157bf7cc4261064471a6db721bf9c83f6
Gitcoins <gitcoins@outlook.com>
2023-06-11 11:57:36 +0800

    auto deploy
    Signed-off-by: Gitcoins <gitcoins@outlook.com>

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# build all libs
+mvn clean install -DskipTests
+
+# install libs
+mkdir kafka
+mkdir kafka/authLibs
+# Copy files to the destination directory
+cp ./oauth-server/target/kafka-oauth-server-1.0.0-SNAPSHOT*.jar ./kafka/authLibs
+cp ./oauth-client/target/kafka-oauth-client-1.0.0-SNAPSHOT*.jar ./kafka/authLibs
+cp ./oauth-keycloak-authorizer/target/kafka-oauth-keycloak-authorizer-1.0.0-SNAPSHOT*.jar ./kafka/authLibs
+cp ./oauth-common/target/kafka-oauth-common-1.0.0-SNAPSHOT*.jar ./kafka/authLibs
+cp ./oauth-common/target/lib/nimbus-jose-jwt-9.10.jar ./kafka/authLibs
+
+# install kafak and config
+# curl -o ./kafka/kafka.tgz https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
+file_path="./kafka/kafka.tgz"
+url="https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz"
+if [ ! -f "$file_path" ]; then
+    curl -o "$file_path" "$url"
+else
+    echo "File already exists: $file_path"
+fi
+
+tar -xzvf ./kafka/kafka.tgz --strip-components=1 -C ./kafka/
+#rm  kafka/kafka_2.13-3.4.0.tgz
+cp ./config/* ./kafka/config/
