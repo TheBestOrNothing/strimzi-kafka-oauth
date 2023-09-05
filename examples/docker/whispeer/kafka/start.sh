a39b0c28980afc25aa06c0da992d9451fb181e45
thebestornothing <gitcoins@outlook.com>
2023-09-05 03:18:53 +0000

    fix: controller can not find the kafka01

--- examples/docker/whispeer/kafka/start.sh
+++ examples/docker/whispeer/kafka/start.sh
@@ -4,21 +4,17 @@ source /etc/profile
 cd /kafka/authLibs/
 
 if ! grep -q "OAUTH_ACCESS_TOKEN=" /etc/profile; then
-#        output=$(java -cp ./*: io.strimzi.kafka.oauth.client.KafkaAdminToken)
     output=$(java -cp ./*: io.strimzi.kafka.oauth.client.Web3AdminToken)
     echo "export OAUTH_ACCESS_TOKEN=\"$output\"" >> /etc/profile
     source /etc/profile
 fi
 
-hostname=$(hostname)
-if ! grep -q "127.0.0.1 $hostname" /etc/hosts; then
-    echo "127.0.0.1 $hostname" >> /etc/hosts
-fi
+#hostname=$(hostname)
+#if ! grep -q "127.0.0.1 $hostname" /etc/hosts; then
+#    echo "127.0.0.1 $hostname" >> /etc/hosts
+#fi
 
 cd /kafka/
-#sleep 3s
-#bin/kafka-server-start.sh -daemon config/server.properties
 bin/kafka-server-start.sh config/server.properties
 
 tail -f /dev/null
-#exec "$@"
--- examples/docker/whispeer/zookeeper/start.sh
+++ examples/docker/whispeer/zookeeper/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-hostname=$(hostname)
-if ! grep -q "127.0.0.1 $hostname" /etc/hosts; then
-    echo "127.0.0.1 $hostname" >> /etc/hosts
-fi
+#hostname=$(hostname)
+#if ! grep -q "127.0.0.1 $hostname" /etc/hosts; then
+#    echo "127.0.0.1 $hostname" >> /etc/hosts
+#fi
 
 source /etc/profile
 /opt/zookeeper/bin/zkServer.sh start
